4368d61550cd27bb1a03c5e6363feb9547fb14fe
Michael0814772 <michael232akin@gmail.com>
2022-07-11 12:32:20 +0100

    first commit

---
+++ src/com/segestina/Main.java
@@ -0,0 +1,15 @@
+package com.segestina;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Hamburger hamburger = new Hamburger("Basic", "Sausage", 3.56, "White");
+        double price = hamburger.itemizeHamburger();
+        hamburger.addHamburgerAddition1("Tomato", 0.27);
+        hamburger.addHamburgerAddition2("Lettuce", 0.75);
+        hamburger.addHamburgerAddition3("Cheese", 1.13);
+        System.out.println("Total item taken is: " + hamburger.itemizeHamburger());
+
+        DeluxeBurger deluxeBurger = new DeluxeBurger();
+    }
+}
